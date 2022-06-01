cbfb0962175c4623120ef377d28fb2f737421cff
Duncan Dean <duncangleeddean@gmail.com>
2022-06-01 21:36:38 +0200

    Initial Commit

---
+++ Tests/SwiftBTCTests/SwiftBTCTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftBTC
+
+final class SwiftBTCTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftBTC().text, "Hello, World!")
+    }
+}
